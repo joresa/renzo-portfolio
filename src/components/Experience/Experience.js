094facb3ab7337f9884c090c5167e49399926195
joresa <renzojohn29@gmai.com>
2023-12-19 07:57:14 -0500

    Configured and tested Tailwind

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,6 +6,10 @@ const Experience = () => {
         <section id="experience">
             <h2>Experience</h2>
             <ExperienceList />
+
+            <div class="text-red-500">This text should be red</div>
+            <div class="bg-blue-200">This div should have a light blue background</div>
+
         </section>
     );
 };
