3e77f658cb4dbe21e5dc6a5535ad5c0a84c5fc14
joresa <renzojohn29@gmai.com>
2023-12-18 21:24:42 -0500

    Add navbar section

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+// src/components/Navbar/Navbar.js
+import React from 'react';
+import './Navbar.css'; // Import the CSS file for styling (to be created)
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <h1 className="logo">Your Logo</h1>
+                <ul className="nav-links">
+                    <li className="nav-link"><a href="#about">About</a></li>
+                    <li className="nav-link"><a href="#experience">Experience</a></li>
+                    <li className="nav-link"><a href="#portfolio">Portfolio</a></li>
+                    <li className="nav-link"><a href="#services">Services</a></li>
+                    <li className="nav-link"><a href="#blog">Blog</a></li>
+                    <li className="nav-link"><a href="#hobbies">Hobbies</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
