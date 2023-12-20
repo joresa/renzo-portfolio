44e038c7589f65eddc7d58f7bec47fb1a7ec2275
joresa <renzojohn29@gmai.com>
2023-12-19 22:34:33 -0500

    Navbar with tailwind + webpack + babel

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,107 @@
-// src/components/Navbar/Navbar.js
-import React from 'react';
-import './Navbar.css'; // Import the CSS file for styling (to be created)
-
+import React from "react";
+import "./Navbar.css"; 
+//import LogoSVG from '../../assets/image/logoipsum-332.svg';
+import { useState } from "react";
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="container">
-                <h1 className="logo">Your Logo</h1>
-                <ul className="nav-links">
-                    <li className="nav-link"><a href="#about">About</a></li>
-                    <li className="nav-link"><a href="#experience">Experience</a></li>
-                    <li className="nav-link"><a href="#portfolio">Portfolio</a></li>
-                    <li className="nav-link"><a href="#services">Services</a></li>
-                    <li className="nav-link"><a href="#blog">Blog</a></li>
-                    <li className="nav-link"><a href="#hobbies">Hobbies</a></li>
-                    <li className="nav-link"><a href="#contact">Contact</a></li>
-                </ul>
-            </div>
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <body className="bg-blue-500">
+      <nav className="relative px-4 py-4 flex justify-between items-center bg-gray-200">
+        <a className="text-3xl font-bold leading-none" href="#Logo">
+        {/* <img src={LogoSVG} alt="Logo" className="h-10" /> */} RENZO
+        </a>
+
+        <div className="lg:hidden">
+          <button className="navbar-burger flex items-cente p-3" onClick={toggleMenu}>
+            <a className="text-3xl font-bold leading-none" href="#Logo">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                </svg>
+            </a>
+          </button>
+        </div>
+
+        {/* Navigation links */}
+        <ul className="hidden lg:flex lg:flex-row lg:items-center lg:space-x-6 justify-end">
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#About"> About</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Experience"> Experience</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Portfolio"> Portfolio</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Services"> Services</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Blog"> Blog</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Hobbies"> Hobbies</a>
+            </li>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Contact"> Contact</a>
+            </li>
+        </ul>
+
+      </nav>
+
+      {/* Mobile menu */}
+      <div className={`navbar-menu ${isMenuOpen ? "relative z-50" : "hidden"}`}>
+        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+          {/* Logo for mobile */}
+        <div className="flex items-center justify-between mb-8">
+            {/* Logo on the left */}
+            <a className="text-3xl font-bold leading-none" href="#Logo">
+               {/* <img src={Logo} alt="Logo" className="h-10" /> */}
+            </a>
+
+            {/* Close button on the right */}
+            <button className="navbar-close" onClick={toggleMenu}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+
+          {/* Mobile menu links */}
+          <div>
+            <ul>
+            <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#About"> About</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Experience"> Experience</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Portfolio"> Portfolio</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Services"> Services</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Blog"> Blog</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Hobbies"> Hobbies</a>
+              </li>
+              <li className="mb-1">
+                <a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#Contact"> Contact</a>
+              </li>
+            </ul>
+          </div>
         </nav>
-    );
+      </div>
+    </body>
+  );
 };
 
 export default Navbar;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      // Add other rules for handling different file types (e.g., SVG) if needed
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
